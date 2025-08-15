a86316568f5d498a98bccd03c530f47c156c05b7
Omar Kobrosli <okobrosli2@gmail.com>
2025-08-15 14:02:18 +0300

    Added local storage functionality.

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,53 @@
+
+
 document.addEventListener('DOMContentLoaded', () =>
 {
-    if(!document.querySelector('.task'))
-    {
-        var container = document.getElementById('empty');
-        container.innerHTML += `<h3> No Tasks Added Yet.</h3>`;
-    }
+    getTasks();
     var add = document.getElementById('add');
-    add.addEventListener('click', () => 
-    {
+    add.addEventListener('click', AddTask)
+       
+    
+})
+
+function getTasks(){
+    const store = 'tasks';
+    const task_list = JSON.parse(localStorage.getItem(store)) || [];
+    var tasks = document.getElementById('tasks');
+    task_list.forEach(task => {
+      
+        var html = `<div class = "task-card">
+            <p class ="task-description"> ${task.description} </p>
+            <p class = "status"> ${task.status} </p>
+            <button class = "edit button"> Edit</button>
+            <button class = "delete button"> Delete</button>
+        </div>`;
+        tasks.innerHTML += html;  
+    });
+}
+
+function AddTask() {
+     const store = 'tasks';
+        const task_list = JSON.parse(localStorage.getItem(store)) || [];
+
+        const new_task = {
+        description: 'hi',
+        status: 'status',
+        };
+        task_list.push(new_task);
+        localStorage.setItem(store,JSON.stringify(task_list));
+        
+        if(!document.querySelector('.task'))
+        {
+            var container = document.getElementById('empty');
+            container.innerHTML += `<h3> No Tasks Added Yet.</h3>`;
+        }
         document.getElementById('empty').style.display = 'none';
         var tasks = document.getElementById('tasks');
-        tasks.innerHTML += `<div class = "task-card">
+        var html = `<div class = "task-card">
             <p class ="task-description"> hi </p>
             <p class = "status"> status </p>
             <button class = "edit button"> Edit</button>
             <button class = "delete button"> Delete</button>
         </div>`;
-    })
-    
-})
-
+        tasks.innerHTML += html;
+}
